07754c3a8985ca786cd574e05a8dcd70fd29820f
Pirush Prechathavanich <pirush@nimbl3.com>
2018-03-08 11:17:36 +0700

    init xcode project

---
+++ {project-name}/{project-name}/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  {project-name}
+//
+//  Created by Pirush Prechathavanich on 3/8/18.
+//  Copyright © 2018 Nimbl3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
