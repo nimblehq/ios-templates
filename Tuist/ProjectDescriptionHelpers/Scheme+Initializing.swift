1c529d80868986447da648d1a645dfbb20e7c326
Phong Vo <phongvhd93@gmail.com>
2021-08-31 15:45:51 +0700

    [179] create variables for names

--- Tuist/ProjectDescriptionHelpers/Scheme+Initializing.swift
+++ Tuist/ProjectDescriptionHelpers/Scheme+Initializing.swift
@@ -4,35 +4,39 @@ extension Scheme {
 
     public static func productionScheme(name: String) -> Scheme {
         #warning("We should use Debug Production and Release Production instead after implement build configurations")
+        let debugConfigName = ProjectBuildConfiguration.debugProduction.name
+        let releaseConfigName = ProjectBuildConfiguration.releaseProduction.name
         return Scheme(
             name: name,
             shared: true,
             buildAction: BuildAction(targets: ["\(name)"]),
             testAction: TestAction(
                 targets: ["\(name)Tests", "\(name)UITests"],
-                configurationName: ProjectBuildConfiguration.debugProduction.name
+                configurationName: debugConfigName
             ),
-            runAction: RunAction(configurationName: ProjectBuildConfiguration.debugProduction.name),
-            archiveAction: ArchiveAction(configurationName: ProjectBuildConfiguration.releaseProduction.name),
-            profileAction: ProfileAction(configurationName: ProjectBuildConfiguration.debugProduction.name),
-            analyzeAction: AnalyzeAction(configurationName: ProjectBuildConfiguration.debugProduction.name)
+            runAction: RunAction(configurationName: debugConfigName),
+            archiveAction: ArchiveAction(configurationName: releaseConfigName),
+            profileAction: ProfileAction(configurationName: debugConfigName),
+            analyzeAction: AnalyzeAction(configurationName: debugConfigName)
         )
     }
 
     public static func stagingScheme(name: String) -> Scheme {
         #warning("We should use Debug Staging and Release Staging instead after implement build configurations")
+        let debugConfigName = ProjectBuildConfiguration.debugStaging.name
+        let releaseConfigName = ProjectBuildConfiguration.releaseStaging.name
         return Scheme(
             name: "\(name) Staging",
             shared: true,
             buildAction: BuildAction(targets: ["\(name)"]),
             testAction: TestAction(
                 targets: ["\(name)Tests", "\(name)UITests"],
-                configurationName: ProjectBuildConfiguration.debugStaging.name
+                configurationName: debugConfigName
             ),
-            runAction: RunAction(configurationName: ProjectBuildConfiguration.debugStaging.name),
-            archiveAction: ArchiveAction(configurationName: ProjectBuildConfiguration.releaseStaging.name),
-            profileAction: ProfileAction(configurationName: ProjectBuildConfiguration.debugStaging.name),
-            analyzeAction: AnalyzeAction(configurationName: ProjectBuildConfiguration.debugStaging.name)
+            runAction: RunAction(configurationName: debugConfigName),
+            archiveAction: ArchiveAction(configurationName: releaseConfigName),
+            profileAction: ProfileAction(configurationName: debugConfigName),
+            analyzeAction: AnalyzeAction(configurationName: debugConfigName)
         )
     }
 }
