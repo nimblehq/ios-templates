ba60367c719f3469d8ffe0dcab699b2271556662
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-04-26 14:41:28 +0700

    [#255] Upgrade to 3.2.0

---
+++ App/Project.swift
@@ -0,0 +1,30 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.project(name: "{PROJECT_NAME}", bundleId: "{BUNDLE_ID_PRODUCTION}")
+
+extension Project {
+
+    static func project(name: String, bundleId: String) -> Project {
+        return Project(
+            name: name,
+            organizationName: "Nimble",
+            options: .options(
+                disableBundleAccessors: true,
+                disableSynthesizedResourceAccessors: true
+            ),
+            settings: .settings(
+                configurations: BuildConfiguration.allCases.map { $0.createConfiguration(projectName: name) }
+            ),
+            targets: [
+                .mainTarget(name: name, bundleId: bundleId),
+                .testsTarget(name: name, bundleId: bundleId),
+                .uiTestsTarget(name: name, bundleId: bundleId)
+            ],
+            schemes: [
+                .productionScheme(name: name),
+                .stagingScheme(name: name)
+            ]
+        )
+    }
+}
