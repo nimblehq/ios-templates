f18847d79f3dd291db6caf7d989e20b6baf282fa
Bliss Wetchayanwiwat <pisit@nimblehq.co>
2022-08-19 17:10:31 +0700

    [#321] Add new workflow and sh script for github testflight test

--- set_up_test_testflight.sh
+++ set_up_test_testflight.sh
@@ -1,11 +1,12 @@
-brew install imagemagick
 fastlane add_plugin appicon
 
 echo "import('./Tests/Fastfile')" | cat - fastlane/Fastfile | tee fastlane/Fastfile &> /dev/null
 
 readonly CONSTANT_API_KEY_ID="<#API_KEY_ID#>"
 readonly CONSTANT_ISSUER_ID="<#ISSUER_ID#>"
-readonly CONSTANT_MATCH_REPO="git@github.com:{organization}/{repo}.git"
+readonly CONSTANT_MATCH_REPO="git@github.com:{organization}\/{repo}.git"
+
+readonly WORKING_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_API_KEY_ID/$API_KEY_ID/g" {} +
 LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_ISSUER_ID/$ISSUER_ID/g" {} +
