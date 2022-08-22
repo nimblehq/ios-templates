2a1f3eedae5fef7b4279b72d67706836272edf7a
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-08-22 10:41:10 +0700

    [#321] Fix url script

--- set_up_test_testflight.sh
+++ set_up_test_testflight.sh
@@ -1,4 +1,4 @@
-fastlane add_plugin appicon
+bundle exec fastlane add_plugin appicon
 
 echo "import('./Tests/Fastfile')" | cat - fastlane/Fastfile | tee fastlane/Fastfile &> /dev/null
 
@@ -7,7 +7,8 @@ readonly CONSTANT_ISSUER_ID="<#ISSUER_ID#>"
 readonly CONSTANT_MATCH_REPO="git@github.com:{organization}\/{repo}.git"
 
 readonly WORKING_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+MATCH_REPO_ESCAPED=$(echo "${MATCH_REPO//\//\\\/}")
 
 LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_API_KEY_ID/$API_KEY_ID/g" {} +
 LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_ISSUER_ID/$ISSUER_ID/g" {} +
-LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_MATCH_REPO/$MATCH_REPO/g" {} +
+LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_MATCH_REPO/$MATCH_REPO_ESCAPED/g" {} +
