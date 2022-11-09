881ed9312c95b81d3453f41facc84f4054c1e671
Su Ho <su.v@nimblehq.co>
2022-11-09 17:15:36 +0700

    [#346] Apply suggestions from code review
    Co-authored-by: Su Nguyen <vnntsu@gmail.com>

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -143,6 +143,7 @@ class Fastfile: LaneFile {
         desc("Set build number with number of commits")
         incrementBuildNumber(
             buildNumber: .userDefined(String(buildNumber)),
-            xcodeproj: .userDefined(Constant.projectPath))
+            xcodeproj: .userDefined(Constant.projectPath)
+        )
     }
 }
