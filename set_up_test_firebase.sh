f14e2cb7fe8a1b85c451dd2f3e3aa99f3caa891b
David <duc@nimblehq.co>
2023-04-11 13:43:18 +0700

    [#471] Update set_up_test_firebase.sh to replace firebase tester groups with nimble

--- set_up_test_firebase.sh
+++ set_up_test_firebase.sh
@@ -1,10 +1,12 @@
 echo "import('./Tests/Fastfile')" | cat - fastlane/Fastfile | tee fastlane/Fastfile &> /dev/null
 
 readonly CONSTANT_STAGING_FIREBASE_APP_ID="<#stagingFirebaseAppId#>"
+readonly CONSTANT_FIREBASE_TESTER_GROUPS=["<#group1#>", "<#group2#>"]
 readonly CONSTANT_MATCH_REPO="git@github.com:{organization}\/{repo}.git"
 
 readonly WORKING_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 MATCH_REPO_ESCAPED=$(echo "${MATCH_REPO//\//\\\/}")
 
 LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_STAGING_FIREBASE_APP_ID/$STAGING_FIREBASE_APP_ID/g" {} +
+LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_FIREBASE_TESTER_GROUPS/nimble/g" {} +
 LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_MATCH_REPO/$MATCH_REPO_ESCAPED/g" {} +
