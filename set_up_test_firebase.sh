9d1eadfc201ce156b9661fed9785e60a843c8ecb
David <duc@nimblehq.co>
2023-04-11 10:10:56 +0700

    [#471] Create Test Upload Build to Firebase workflow
    Create set_up_test_firebase.sh file
    Update test upload build to firebase workflow to us latest macOS

---
+++ set_up_test_firebase.sh
@@ -0,0 +1,10 @@
+echo "import('./Tests/Fastfile')" | cat - fastlane/Fastfile | tee fastlane/Fastfile &> /dev/null
+
+readonly CONSTANT_STAGING_FIREBASE_APP_ID="<#stagingFirebaseAppId#>"
+readonly CONSTANT_MATCH_REPO="git@github.com:{organization}\/{repo}.git"
+
+readonly WORKING_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+MATCH_REPO_ESCAPED=$(echo "${MATCH_REPO//\//\\\/}")
+
+LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_STAGING_FIREBASE_APP_ID/$STAGING_FIREBASE_APP_ID/g" {} +
+LC_ALL=C find $WORKING_DIR -type f -exec sed -i "" "s/$CONSTANT_MATCH_REPO/$MATCH_REPO_ESCAPED/g" {} +
