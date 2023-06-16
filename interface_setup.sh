898d562dd0406e98170672b1d68d9d2809b1e450
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-16 15:01:32 +0700

    [#483] Use cmd argument

--- interface_setup.sh
+++ interface_setup.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
-if [[ -z "${CI}" ]]; then
-    read -p "Which Interface do you want to use [(S)wiftUI/(U)IKit]: " interface
 
-    if [ "$interface" = "s" -o "$interface" = "SwiftUI" -o "$interface" = "S" ]; then
-        echo "Setting up SwiftUI" 
-        cp -r tuist/Interfaces/SwiftUI/Pod/. .
-        cp -r tuist/Interfaces/SwiftUI/Sources/. {PROJECT_NAME}/Sources
-    else
-        echo "Setting up UIKit"
-        cp -r tuist/Interfaces/UIKit/Pod/. .
-        cp -r tuist/Interfaces/UIKit/Sources/. {PROJECT_NAME}/Sources
-    fi
+interface=""
+
+while [ $# -gt 0 ] ; do
+    case "$1" in
+    -i|--interface)
+        interface="$2"
+        shift
+        ;;
+    esac
+    shift
+done
+
+if [ "$interface" = "s" -o "$interface" = "SwiftUI" -o "$interface" = "S" ]; then
+    echo "Setting up SwiftUI" 
+    cp -r tuist/Interfaces/SwiftUI/Pod/. .
+    cp -r tuist/Interfaces/SwiftUI/Sources/. {PROJECT_NAME}/Sources
 else
+    echo "Setting up UIKit"
     cp -r tuist/Interfaces/UIKit/Pod/. .
     cp -r tuist/Interfaces/UIKit/Sources/. {PROJECT_NAME}/Sources
 fi
--- make.sh
+++ make.sh
@@ -24,6 +24,7 @@ Options:
 -b, --bundle-id [BUNDLE_ID_PRODUCTION]       the production id (i.e. com.example.package)
 -s, --bundle-id-staging [BUNDLE_ID_STAGING]  the staging id (i.e. com.example.package.staging)
 -n, --project-name [PROJECT_NAME]            the project name (i.e. MyApp)
+-i, --interface [INTERFACE]                  the user interface frameword (SwiftUI or UIKit)
 EOF
     exit 1
 }
@@ -32,6 +33,7 @@ bundle_id_production=""
 bundle_id_staging=""
 project_name=""
 minimum_version=""
+interface=""
 
 readonly CONSTANT_PROJECT_NAME="{PROJECT_NAME}"
 readonly CONSTANT_BUNDLE_PRODUCTION="{BUNDLE_ID_PRODUCTION}"
@@ -53,6 +55,10 @@ while [ $# -gt 0 ] ; do
         ;;
     -n|--project-name)
         project_name="$2"
+        shift
+        ;;
+    -i|--interface)
+        interface="$2"
         shift
         ;;
     -*)
@@ -84,6 +90,13 @@ if [ -z "$minimum_version" ]; then
     minimum_version="14.0"
 fi
 
+if [ -z "$interface" ]; then
+    read -p "Interface [(S)wiftUI or (U)IKit]:" interface
+    if [ -z "$interface" ]; then
+        interface="SwiftUI"
+    fi
+fi
+
 # Enforce minimum version
 version_regex='^[0-9_]+(\.[0-9]+)+$'
 if ! [[ $minimum_version =~ $version_regex ]]; then
@@ -102,7 +115,7 @@ if ! [[ $bundle_id_production =~ $regex ]]; then
 fi
 
 # Choose the Interface
-sh interface_setup.sh
+sh interface_setup.sh -i $interface
 
 echo "=> 🐢 Starting init $project_name ..."
 
